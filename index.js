d9de8f11c600533fca748bc56f850860b1bceeac
SHOJOL-ISLAM-77 <sh0j0l3875891@gmail.com>
2024-01-11 23:19:43 +0600

    add create project function

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-
-
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId, } = require("mongodb");
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5002;  
+const port = process.env.PORT || 7000;
+const jwt = require("jsonwebtoken");
 
-app.use(cors());
-app.use(express.json()); 
+// middleware
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://remotely-d0697.web.app"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b2jpujy.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -23,46 +30,120 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // await client.connect();
-    const userCollecction = client.db("remotely").collection("users"); 
-    // const cartCollection = client.db("productDB").collection("carts"); 
+    const userCollection = client.db("remotely").collection("users");
+    const skillsCollection = client.db("remotely").collection("skills");
+    const timeZoneCollection = client.db("remotely").collection("timeZone");
+    const userProjectCollection = client
+      .db("remotely")
+      .collection("userProject");
 
-    app.post("/users", async (req, res) => { 
+    app.post("/api/v1/create-jwt", async (req, res) => {
       const user = req.body;
-      const query = { email: user?.email }; 
-      const existingUser = await userCollecction.findOne(query);    
-      if (existingUser) { 
-        return res.send({ message: "User already exists", insertedId: null }); 
-      } 
- 
-      const result = await userCollecction.insertOne(user);  
-      res.send(result); 
+      console.log("I need a new jwt", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
     });
-    
 
-    app.get("/users/:email", async (req, res) => { 
-      const { email } = req.params; // Access email from route parameters
-      const query = { email: email }; 
-      const user = await userCollecction.findOne(query)
-      if(user){
-       return  res.json(user); 
+    // Logout
+    app.get("/logout", async (req, res) => {
+      try {
+        res
+          .clearCookie("token", {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          })
+          .send({ success: true });
+        console.log("Logout successful");
+      } catch (err) {
+        res.status(500).send(err);
       }
-      else{
-        res.status(404).json({ message: 'User not found' }); 
-      } 
-      res.send(user); 
-    }); 
+    });
 
+    // User projects related api
 
-    // app.post("/products", async (req, res) => {
-    //   const product = req.body;
-    //   const result = await productCollection.insertOne(product);
-    //   res.send(result);  
-    // });
+    app.post("/api/v1/create-project", async (req, res) => {
+      const data = req.body;
+      const result = await userProjectCollection.insertOne(data);
+      res.send(result);
+    });
+
+    // User related apis
+
+    // Save or modify user email, status in DB
+    app.put("/api/v1/create-user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email: email };
+      const options = { upsert: true };
+      const isExist = await userCollection.findOne(query);
+      console.log("User found?----->", isExist);
+      if (isExist) return res.send(isExist);
+      const result = await userCollection.updateOne(
+        query,
+        {
+          $set: { ...user },
+        },
+        options
+      );
+      res.send(result);
+    });
+
+    app.get("/api/v1/get-user/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.put("/api/v1/update-user/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = {
+        email: email,
+      };
+      const userData = req.body;
+      const options = { upsert: true };
+
+      const result = await userCollection.updateOne(
+        filter,
+        {
+          $set: { ...userData },
+        },
+        options
+      );
+      res.send(result);
+    });
+
+    app.get("/api/v1/get-timeZone", async (req, res) => {
+      const result = await timeZoneCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/api/v1/get-skills/:id", async (req, res) => {
+      const id = req.params.id;
+      if (id != 0) {
+        const query = { skill_id: id };
+        const result = await skillsCollection.find(query).toArray();
+        res.send(result);
+      }
+      if (id == 0) {
+        const result = await skillsCollection.find().toArray();
+        res.send(result);
+      }
+    });
 
     // await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // await client.close();
   }
@@ -74,8 +155,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(` remotely is running on port: ${port}`); 
+  console.log(` remotely is running on port: ${port}`);
 });
-
-
-
